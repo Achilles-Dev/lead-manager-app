08203ed894613811175127dee43442b02b421a76
Achilles <solohag92@gmail.com>
2025-03-06 16:54:40 +0000

    Fix delpoy issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-import { baseURL, getLeads } from "@/api/lead";
+import { getLeads } from "@/api/lead";
 import AllLeads from "./(lead)/AllLeads";
 
 export default async function Home() {
-  let leads = [];
-  if (baseURL !== undefined) {
-    leads = await getLeads();
-  }
+  const leads = await getLeads();
 
   return (
     <div className="h-screen">
